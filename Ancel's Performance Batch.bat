9baabe250f5646b51490d9c04b7fd14566ee2ae4
ancel <99452431+ancel1x@users.noreply.github.com>
2023-02-05 20:03:55 -0500

    Version 1.0

--- Ancel's Performance Batch.bat
+++ Ancel's Performance Batch.bat
@@ -866,12 +866,12 @@ timeout /t 1 /nobreak > NUL
 
 :: Mouse Data Queue Size
 echo Setting Mouse Data Queue Size
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /v "MouseDataQueueSize" /t REG_DWORD /d "10" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /v "MouseDataQueueSize" /t REG_DWORD /d "16" /f >> APB_Log.txt
 timeout /t 1 /nobreak > NUL
 
 :: Keyboard Data Queue Size
 echo Setting Keyboard Data Queue Size
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v "KeyboardDataQueueSize" /t REG_DWORD /d "10" /f >> APB_Log.txt
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v "KeyboardDataQueueSize" /t REG_DWORD /d "16" /f >> APB_Log.txt
 timeout /t 1 /nobreak > NUL
 
 :: DebugPollInterval (BeersE#9366)
