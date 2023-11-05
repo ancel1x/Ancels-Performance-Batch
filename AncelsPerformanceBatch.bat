e5f30eb8af132bf2b81a57eeb897bdcd576431d6
MiRw3b <60517683+MiRw3b@users.noreply.github.com>
2023-11-05 14:28:33 +0000

    removed DalDram thing cause it causes flickers on amd gpus

--- AncelsPerformanceBatch.bat
+++ AncelsPerformanceBatch.bat
@@ -1029,8 +1029,8 @@ timeout /t 1 /nobreak > NUL
 echo Applying Melody AMD Tweaks
 for %%a in (LTRSnoopL1Latency LTRSnoopL0Latency LTRNoSnoopL1Latency LTRMaxNoSnoopLatency KMD_RpmComputeLatency
         DalUrgentLatencyNs memClockSwitchLatency PP_RTPMComputeF1Latency PP_DGBMMMaxTransitionLatencyUvd
-        PP_DGBPMMaxTransitionLatencyGfx DalNBLatencyForUnderFlow DalDramClockChangeLatencyNs
-        BGM_LTRSnoopL1Latency BGM_LTRSnoopL0Latency BGM_LTRNoSnoopL1Latency BGM_LTRNoSnoopL0Latency
+        PP_DGBPMMaxTransitionLatencyGfx DalNBLatencyForUnderFlow BGM_LTRSnoopL1Latency 
+        BGM_LTRSnoopL0Latency BGM_LTRNoSnoopL1Latency BGM_LTRNoSnoopL0Latency
         BGM_LTRMaxSnoopLatencyValue BGM_LTRMaxNoSnoopLatencyValue) do (reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "%%a" /t REG_DWORD /d "1" /f >> APB_Log.txt
 )
 
